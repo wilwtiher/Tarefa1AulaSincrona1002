d776b93ab03093f21b890a7df890d05cdae91043
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-02-13 10:30:35 -0300

    configuracao basica de cada funcionalidade

--- tarefa.c
+++ tarefa.c
@@ -6,31 +6,78 @@
 #include "hardware/clocks.h"
 #include "inc/ssd1306.h"
 #include "inc/font.h"
+#include "hardware/adc.h"
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+// para o I2C
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
 
+// para o DISPLAY
+#define IS_RGBW false
+#define NUM_PIXELS 25
 
+// Pinos
+#define led_RED 13   // Red=13, Blue=12, Green=11
+#define led_BLUE 12  // Red=13, Blue=12, Green=11
+#define led_GREEN 11 // Red=13, Blue=12, Green=11
+#define botao_pinA 5 // Botão A = 5, Botão B = 6 , BotãoJoy = 22
+#define botao_pinB 6 // Botão A = 5, Botão B = 6 , BotãoJoy = 22
+#define VRX_PIN 26   // Pino do Joystick X
+
+// Variáveis globais
+static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
+static volatile int contador = 0;
+bool cor = true;
+bool Vermelho = false;
+bool Verde = false;
+bool Azul = false;
+
+// Funcao para iniciar pino PWM
+uint pwm_init_gpio(uint gpio, uint wrap)
+{
+    gpio_set_function(gpio, GPIO_FUNC_PWM);
+
+    uint slice_num = pwm_gpio_to_slice_num(gpio);
+    pwm_set_wrap(slice_num, wrap);
+
+    pwm_set_enabled(slice_num, true);
+    return slice_num;
+}
 
 int main()
 {
     stdio_init_all();
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+    adc_init(); 
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    adc_gpio_init(VRX_PIN); 
+
+    uint pwm_wrap = 4096;  
+    uint pwm_slice = pwm_init_gpio(led_BLUE, pwm_wrap);
+    uint pwm_slice = pwm_init_gpio(led_GREEN, pwm_wrap);
+    
+    uint32_t last_print_time = 0; 
+
+
+    // Para o display
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
+    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
+    ssd1306_t ssd;                                                // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd);                                         // Configura o display
+    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+
+    while (true)
+    {
+
     }
 }
