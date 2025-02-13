8c224eeb79bcde8482efddabc4e7bf659043983c
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-02-13 10:55:45 -0300

    testes simples de joystick

--- tarefa.c
+++ tarefa.c
@@ -76,8 +76,22 @@ int main()
     ssd1306_fill(&ssd, false);
     ssd1306_send_data(&ssd);
 
-    while (true)
-    {
+    while (true) {
+        adc_select_input(0);  
+        uint16_t vrx_value = adc_read(); 
 
+        pwm_set_gpio_level(led_RED, vrx_value); 
+
+        float duty_cycle = (vrx_value / 4095.0) * 100;  
+
+        
+        uint32_t current_time = to_ms_since_boot(get_absolute_time());  
+        if (current_time - last_print_time >= 1000) {  
+            printf("VRX: %u\n", vrx_value); 
+            printf("Duty Cycle LED: %.2f%%\n", duty_cycle); 
+            last_print_time = current_time;  
+        }
+
+        sleep_ms(100);  
     }
 }
