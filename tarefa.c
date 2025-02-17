f300a24e9b6d7c801a83712449e060da2fc12afb
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-02-17 09:13:06 -0300

    commit final2

--- tarefa.c
+++ tarefa.c
@@ -29,7 +29,6 @@
 
 // Variáveis globais
 static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
-static volatile int contador = 0;
 bool cor = true;
 bool display = true;
 bool LEDS = true;
